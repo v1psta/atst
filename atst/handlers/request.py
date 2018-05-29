66ff2232043631be9e6485cfde5171176f598824
Luis Cielak <luis+dds@promptworks.com>
2018-05-29 15:19:46 -0400

    Add nested menu items for details of use and add placeholder requests screens

--- atst/handlers/request.py
+++ atst/handlers/request.py
@@ -2,9 +2,21 @@ from atst.handler import BaseHandler
 
 class Request(BaseHandler):
     screens = [
-            { 'title' : 'Application Details', },
-            { 'title' : 'Computation', },
-            { 'title' : 'Storage', }
+            { 'title' : 'Details of Use', 
+              'subitems' : [
+                {'title' : 'Application Details',
+                 'id' : 'application-details'},
+                {'title' : 'Computation',
+                  'id' : 'computation' },
+                {'title' : 'Storage',
+                  'id' : 'storage' },
+                {'title' : 'Usage',
+                  'id' : 'usage' },
+            ]},
+            { 'title' : 'Organizational Info', },
+            { 'title' : 'Funding/Contracting', },
+            { 'title' : 'Readiness Survey', },
+            { 'title' : 'Review & Submit', }
      ]
 
     def initialize(self, page):
