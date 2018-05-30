5667aedce404cf02a44bf86ebcdc28d011935a8a
Luis Cielak <luis+dds@promptworks.com>
2018-05-30 10:40:14 -0400

    Add existing requests table to reqs page

--- atst/app.py
+++ atst/app.py
@@ -8,8 +8,9 @@ def make_app(**kwargs):
     app = tornado.web.Application([
             url( r"/",           MainHandler, {'page': 'login'},       name='login' ),
             url( r"/home",       MainHandler, {'page': 'home'},       name='home' ),
-            url( r"/requests",   Request,     {'page': 'requests'},   name='requests' ),
             url( r"/accounts",   MainHandler, {'page': 'accounts'},   name='accounts' ),
+            url( r"/requests",   Request,     {'page': 'requests'},   name='requests' ),
+            url( r"/requests/new",   Request, {'page': 'requests_new'},   name='request_new' ),
             url( r"/requests/([0-9])", Request, {'page': 'requests'}, name='request_form' ),
             url( r"/users",      MainHandler, {'page': 'users'},      name='users' ),
             url( r"/reports",    MainHandler, {'page': 'reports'},    name='reports' ),
