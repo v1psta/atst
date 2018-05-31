2732ccfb1249bd58a9f505a4681e003d379fe31e
Brian Duggan <brian+dds@promptworks.com>
2018-05-31 10:51:23 -0400

    Move mock data into server

--- atst/handlers/request.py
+++ atst/handlers/request.py
@@ -1,8 +1,35 @@
 from atst.handler import BaseHandler
 
+mock_requests = [
+        {
+            'order_id'  : 36552612,
+            'date'      : '5/17/2018',
+            'is_new'    : True,
+            'full_name' : 'Friedrich Straat',
+            'app_count' : 2,
+            'status'    : 'Pending'
+        },
+        {
+            'order_id'  : 87362910,
+            'date'      : '10/2/2017',
+            'is_new'    : False,
+            'full_name' : 'Pietro Quirinis',
+            'app_count' : 1,
+            'status'    : 'Complete'
+        },
+        {
+            'order_id'  : 29938172,
+            'date'      : '1/7/2017',
+            'is_new'    : False,
+            'full_name' : 'Marina Borsetti',
+            'app_count' : 1,
+            'status'    : 'Denied'
+        },
+        ]
+
 class Request(BaseHandler):
     def initialize(self, page):
         self.page = page
 
     def get(self):
-        self.render('requests.html.to', page = self.page)
+        self.render('requests.html.to', page = self.page, requests = mock_requests )
