73eb21a8632a83efb3cc21e83167e60ce51e3e18
richard-dds <richard-ctr@friends.dds.mil>
2018-07-09 13:13:30 -0400

    Only allow letters in POC name fields

--- atst/forms/poc.py
+++ atst/forms/poc.py
@@ -1,16 +1,14 @@
 from wtforms.fields import StringField
 from wtforms.validators import Required, Email, Length
 from wtforms_tornado import Form
-from .validators import IsNumber
+from .validators import IsNumber, Alphabet
 
 
 class POCForm(Form):
-    fname_poc = StringField("POC First Name", validators=[Required()])
-    lname_poc = StringField("POC Last Name", validators=[Required()])
+    fname_poc = StringField("POC First Name", validators=[Required(), Alphabet()])
+    lname_poc = StringField("POC Last Name", validators=[Required(), Alphabet()])
 
-    email_poc = StringField(
-        "POC Email Address", validators=[Required(), Email()]
-    )
+    email_poc = StringField("POC Email Address", validators=[Required(), Email()])
 
     dodid_poc = StringField(
         "DOD ID", validators=[Required(), Length(min=10), IsNumber()]
