46a8d8aade5ccc08d39eae1b44f5b763be2c0168
dandds <dan-ctr@friends.dds.mil>
2018-07-18 10:45:26 -0400

    add additional dev roles and set perms for them

--- atst/handlers/dev.py
+++ atst/handlers/dev.py
@@ -13,8 +13,27 @@ _DEV_USERS = {
         "first_name": "Olivia",
         "last_name": "Owner",
     },
+    "admin": {
+        "id": "66ebf7b8-cbf0-4ed8-a102-5f105330df75",
+        "first_name": "Andreas",
+        "last_name": "Admin",
+    },
+    "developer": {
+        "id": "7707b9f2-5945-49ae-967a-be65baa88baf",
+        "first_name": "Dominick",
+        "last_name": "Developer",
+    },
+    "billing_auditor": {
+        "id": "6978ac0c-442a-46aa-a0c3-ff17b5ec2a8c",
+        "first_name": "Billie",
+        "last_name": "The Billing Auditor",
+    },
+    "security_auditor": {
+        "id": "596fd001-bb1d-4adf-87d8-fa2312e882de",
+        "first_name": "Sawyer",
+        "last_name": "The Security Auditor",
+    },
 }
-
 
 class Dev(BaseHandler):
 
@@ -27,4 +46,12 @@ class Dev(BaseHandler):
     def get(self):
         role = self.get_argument("role", "ccpo")
         user = _DEV_USERS[role]
+        yield self._set_user_permissions(user["id"], role)
         yield self.login(user)
+
+    @tornado.gen.coroutine
+    def _set_user_permissions(self, user_id, role):
+        response = yield self.authz_client.post(
+            "/users", json={"id": user_id, "atat_role": role}
+        )
+        return response.json
