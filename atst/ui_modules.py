6ebbdfc64d3eb9d2eb34489f76a4bd623a592c26
Andrew Croce <andrew+dds@promptworks.com>
2018-07-20 08:23:19 -0400

    Sidenav Item module

--- atst/ui_modules.py
+++ atst/ui_modules.py
@@ -5,3 +5,12 @@ class Icon(UIModule):
         with open('static/icons/%s.svg' % name) as svg:
             return self.render_string(
               "components/icon.html.to", svg=svg.read(), name=name, classes=classes)
+
+class SidenavItem(UIModule):
+    def render(self, label, href, active=False, icon=None):
+        return self.render_string(
+          "navigation/_sidenav_item.html.to",
+          label=label,
+          href=href,
+          active=active,
+          icon=icon)
