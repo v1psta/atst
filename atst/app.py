264273e66b9eb73d9a1e59114ee53b0fcd306ccf
richard-dds <richard-ctr@friends.dds.mil>
2018-07-23 15:23:09 -0400

    Requests page working

--- atst/app.py
+++ atst/app.py
@@ -17,6 +17,7 @@ from atst.api_client import ApiClient
 from atst.sessions import RedisSessions
 from atst import ui_modules
 from atst import ui_methods
+from atst.database import make_db
 
 ENV = os.getenv("TORNADO_ENV", "dev")
 
@@ -52,7 +53,7 @@ def make_app(config, deps, **kwargs):
         url(
             r"/requests",
             Request,
-            {"page": "requests", "requests_client": deps["requests_client"]},
+            {"page": "requests", "db_session": deps["db_session"]},
             name="requests",
         ),
         url(
@@ -130,6 +131,7 @@ def make_deps(config):
     )
 
     return {
+        "db_session": make_db(config),
         "authz_client": ApiClient(
             config["default"]["AUTHZ_BASE_URL"],
             api_version="v1",
