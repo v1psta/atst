c6636bec780ef5028f33f73920f334eedc18b34e
richard-dds <richard-ctr@friends.dds.mil>
2018-07-23 14:58:26 -0400

    Models and migrations for requests

--- atst/models/request_status_event.py
+++ atst/models/request_status_event.py
@@ -3,15 +3,15 @@ from sqlalchemy.types import DateTime, String, BigInteger
 from sqlalchemy.schema import Sequence
 from sqlalchemy.dialects.postgresql import UUID
 
-from requests_queue.models import Base
-from requests_queue.models.types import Id
+from atst.models import Base
+from atst.models.types import Id
 
 
-class StatusEvent(Base):
-    __tablename__ = 'status_events'
+class RequestStatusEvent(Base):
+    __tablename__ = 'request_status_events'
 
     id = Id()
     new_status = Column(String())
     time_created = Column(DateTime(timezone=True), server_default=func.now())
     request_id = Column(UUID(as_uuid=True), ForeignKey('requests.id', ondelete='CASCADE'))
-    sequence = Column(BigInteger, Sequence('status_events_sequence_seq'), nullable=False)
+    sequence = Column(BigInteger, Sequence('request_status_events_sequence_seq'), nullable=False)
