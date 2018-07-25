ac6ce73cc2b8f70b3d56c0fe8120db014822c117
Luis Cielak <luis+dds@promptworks.com>
2018-07-25 15:51:02 -0400

    Update fields for details of use section

--- atst/forms/request.py
+++ atst/forms/request.py
@@ -10,7 +10,6 @@ import pendulum
 class RequestForm(ValidatedForm):
 
     # Details of Use: General
-
     dod_component = StringField(
         "DoD Component",
         description="Identify the DoD component that is requesting access to the JEDI Cloud",
@@ -21,87 +20,42 @@ class RequestForm(ValidatedForm):
         description="Briefly describe how you are expecting to use the JEDI Cloud",
     )
 
-
     # Details of Use: Cloud Readiness
-
-
-
-
-
-
-
-
-
-
-
-    # Details of Use: Overall Request Details
-    dollar_value = IntegerField(
-        "What is the total estimated dollar value of the cloud resources you are requesting using the <a href=\"#\" target=\"_blank\">JEDI CSP Calculator</a>?",
-        validators=[InputRequired(), NumberRange(min=1)],
+    num_software_systems = IntegerField(
+        "Number of Software System",
+        description="Estimate the number of software systems that will be supported by this JEDI Cloud access request",
     )
 
-    num_applications = IntegerField(
-        "Estimate the number of applications that might be supported by this request",
-        validators=[InputRequired(), NumberRange(min=1)],
-    )
-
-    date_start = DateField(
-        "Date you expect to start accessing this cloud resource.",
-        validators=[
-            InputRequired(),
-            DateRange(
-                lower_bound=pendulum.duration(days=0),
-                message="Must be no earlier than today.",
-            ),
-        ],
-    )
-
-    app_description = TextAreaField(
-        "Describe how your team is expecting to use the JEDI Cloud"
-    )
-
-    supported_organizations = StringField(
-        "What organizations are supported by these applications?",
-        validators=[InputRequired()],
-    )
-
-    # Details of Use: Cloud Resources
-    total_cores = IntegerField(
-        "Total Number of vCPU cores", validators=[InputRequired(), NumberRange(min=0)]
-    )
-    total_ram = IntegerField(
-        "Total RAM", validators=[InputRequired(), NumberRange(min=0)]
-    )
-    total_object_storage = IntegerField(
-        "Total object storage", validators=[InputRequired(), NumberRange(min=0)]
-    )
-    total_database_storage = IntegerField(
-        "Total database storage", validators=[InputRequired(), NumberRange(min=0)]
-    )
-    total_server_storage = IntegerField(
-        "Total server storage", validators=[InputRequired(), NumberRange(min=0)]
-    )
-
-    # Details of Use: Support Staff
-    has_contractor_advisor = RadioField(
-        "Do you have a contractor to advise and assist you with using cloud services?",
+    jedi_migration = RadioField(
+        "Are you using the JEDI Cloud to migrate existing systems?",
         choices=[("yes", "Yes"), ("no", "No")],
-        validators=[InputRequired()],
     )
 
-    is_migrating_application = RadioField(
-        "Are you using the JEDI Cloud to migrate existing applications?",
+    rationalization_software_systems = RadioField(
+        "Have you completed a “rationalization” of your software systems to move to the cloud?",
+        choices=[("yes", "Yes"), ("no", "No"), ("in_progress","In Progress")],
+    )
+
+    technical_support_team = RadioField(
+        "Are you working with a technical support team experienced in cloud migrations?",
         choices=[("yes", "Yes"), ("no", "No")],
-        validators=[InputRequired()],
     )
 
-    has_migration_office = RadioField(
-        "Do you have a migration office that you're working with to migrate to the cloud?",
+    organization_providing_assistance = RadioField(
+        "If you are receiving migration assistance, indicate the type of organization providing assistance below:",
         choices=[("yes", "Yes"), ("no", "No")],
-        validators=[InputRequired()],
     )
 
-    supporting_organization = StringField(
-        "Describe the organizations that are supporting you, include both government and contractor resources",
-        validators=[InputRequired()],
-    )
+
+
+    # Organization Providing Assistance
+
+    # # Engineering Assessment
+
+    # # Data Transfers
+
+    # # Expected Completion Date
+
+    # # Cloud Native
+
+
