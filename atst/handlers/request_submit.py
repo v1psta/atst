94fb61682c0d57594e319d0cd62a807d0511a513
dandds <dan-ctr@friends.dds.mil>
2018-07-27 11:19:48 -0400

    show next-steps modal when request is auto-approved

--- atst/handlers/request_submit.py
+++ atst/handlers/request_submit.py
@@ -14,4 +14,16 @@ class RequestsSubmit(BaseHandler):
             "/requests/{}/submit".format(request_id),
             allow_nonstandard_methods=True
         )
-        self.redirect("/requests")
+        approved = yield self._check_approved(request_id)
+        if approved:
+            self.redirect("/requests?modal=True")
+        else:
+            self.redirect("/requests")
+
+    @tornado.gen.coroutine
+    def _check_approved(self, request_id):
+        response = yield self.requests_client.get(
+            "/requests/{}".format(request_id)
+        )
+        status = response.json.get("status")
+        return status == "approved"
