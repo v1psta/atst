c02a95d00a686daf26aff0e1000a4d43df24381b
dandds <dan-ctr@friends.dds.mil>
2018-07-30 14:46:37 -0400

    import fundz models

---
+++ atst/models/task_order.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+
+from atst.models import Base
+
+class TaskOrder(Base):
+    __tablename__ = "task_order"
+
+    id = Column(Integer, primary_key=True)
+    number = Column(String)
