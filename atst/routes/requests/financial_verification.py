177ae84e8a7cbe80e446f18d7db18ecb11c77a95
richard-dds <richard-ctr@friends.dds.mil>
2018-08-02 13:11:21 -0400

    Split requests routes into multiple files

---
+++ atst/routes/requests/financial_verification.py
@@ -0,0 +1,40 @@
+from flask import render_template, redirect, url_for
+
+from . import requests_bp
+from atst.domain.requests import Requests
+from atst.forms.financial import FinancialForm
+
+@requests_bp.route("/requests/verify/<string:request_id>", methods=["GET"])
+def financial_verification(request_id=None):
+    request = Requests.get(request_id)
+    form = FinancialForm(data=request.body.get("financial_verification"))
+    return render_template(
+        "requests/financial_verification.html", f=form, request_id=request_id
+    )
+
+
+@requests_bp.route("/requests/verify/<string:request_id>", methods=["POST"])
+def update_financial_verification(request_id):
+    post_data = http_request.form
+    existing_request = Requests.get(request_id)
+    form = FinancialForm(post_data)
+
+    rerender_args = dict(request_id=request_id, f=form)
+
+    if form.validate():
+        request_data = {"financial_verification": post_data}
+        Requests.update(request_id, request_data)
+        valid = form.perform_extra_validation(
+            existing_request.body.get("financial_verification")
+        )
+        if valid:
+            redirect(url_for("requests.financial_verification_submitted"))
+        else:
+            return render_template("requests/financial_verification.html", **rerender_args)
+    else:
+        return render_template("requests/financial_verification.html", **rerender_args)
+
+
+@requests_bp.route("/requests/financial_verification_submitted")
+def financial_verification_submitted():
+    pass
