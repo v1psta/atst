fef66f335e66ca30935b7fe33d00cc171698e53e
richard-dds <richard-ctr@friends.dds.mil>
2018-08-06 14:27:35 -0400

    requests fixture no longer needed

--- tests/domain/test_requests.py
+++ tests/domain/test_requests.py
@@ -7,10 +7,6 @@ from atst.domain.requests import Requests
 from tests.factories import RequestFactory
 
 
-@pytest.fixture()
-def requests(session):
-    return Requests()
-
 @pytest.fixture(scope="function")
 def new_request(session):
     created_request = RequestFactory.create()
@@ -20,33 +16,33 @@ def new_request(session):
     return created_request
 
 
-def test_can_get_request(requests, new_request):
-    request = requests.get(new_request.id)
+def test_can_get_request(new_request):
+    request = Requests.get(new_request.id)
 
     assert request.id == new_request.id
 
 
-def test_nonexistent_request_raises(requests):
+def test_nonexistent_request_raises():
     with pytest.raises(NotFoundError):
-        requests.get(uuid4())
+        Requests.get(uuid4())
 
 
-def test_auto_approve_less_than_1m(requests, new_request):
+def test_auto_approve_less_than_1m(new_request):
     new_request.body = {"details_of_use": {"dollar_value": 999999}}
-    request = requests.submit(new_request)
+    request = Requests.submit(new_request)
 
     assert request.status == 'approved'
 
 
-def test_dont_auto_approve_if_dollar_value_is_1m_or_above(requests, new_request):
+def test_dont_auto_approve_if_dollar_value_is_1m_or_above(new_request):
     new_request.body = {"details_of_use": {"dollar_value": 1000000}}
-    request = requests.submit(new_request)
+    request = Requests.submit(new_request)
 
     assert request.status == 'submitted'
 
 
-def test_dont_auto_approve_if_no_dollar_value_specified(requests, new_request):
+def test_dont_auto_approve_if_no_dollar_value_specified(new_request):
     new_request.body = {"details_of_use": {}}
-    request = requests.submit(new_request)
+    request = Requests.submit(new_request)
 
     assert request.status == 'submitted'
