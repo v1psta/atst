b8fe460ccd148be66895d08eda5c361cebf87e72
dandds <dan-ctr@friends.dds.mil>
2018-08-06 09:18:53 -0400

    extract get_current_user, fix tests

--- atst/domain/auth.py
+++ atst/domain/auth.py
@@ -8,11 +8,19 @@ def login_required(f):
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user_id"):
-            g.user = Users.get(session.get("user_id"))
+        user = get_current_user()
+        if user:
+            g.user = user
             return f(*args, **kwargs)
 
         else:
             return redirect(url_for("atst.root"))
 
     return decorated_function
+
+def get_current_user():
+    user_id = session.get("user_id")
+    if user_id:
+        return Users.get(user_id)
+    else:
+        return False
--- tests/mocks.py
+++ tests/mocks.py
@@ -19,6 +19,12 @@ MOCK_REQUEST = RequestFactory.create(
         },
     }
 )
+DOD_SDN_INFO = {
+        'first_name': 'ART',
+        'last_name': 'GARFUNKEL',
+        'dod_id': '5892460358'
+    }
+DOD_SDN = f"CN={DOD_SDN_INFO['last_name']}.{DOD_SDN_INFO['first_name']}.G.{DOD_SDN_INFO['dod_id']},OU=OTHER,OU=PKI,OU=DoD,O=U.S. Government,C=US"
 
 
 class MockApiClient(ApiClient):
