04b9ae9f5364e4c7c02aa5159c739fa87b1511f2
richard-dds <richard-ctr@friends.dds.mil>
2018-08-07 21:32:47 -0400

    Display creator's name and human-readable status

--- atst/routes/requests/index.py
+++ atst/routes/requests/index.py
@@ -5,17 +5,17 @@ from . import requests_bp
 from atst.domain.requests import Requests
 
 
-def map_request(user, request):
+def map_request(request):
     time_created = pendulum.instance(request.time_created)
     is_new = time_created.add(days=1) > pendulum.now()
 
     return {
         "order_id": request.id,
         "is_new": is_new,
-        "status": request.status,
+        "status": request.status.name.capitalize(),
         "app_count": 1,
         "date": time_created.format("M/DD/YYYY"),
-        "full_name": user.full_name
+        "full_name": request.creator.full_name
     }
 
 
@@ -30,6 +30,6 @@ def requests_index():
     else:
         requests = Requests.get_many(creator=g.current_user)
 
-    mapped_requests = [map_request(g.current_user, r) for r in requests]
+    mapped_requests = [map_request(r) for r in requests]
 
     return render_template("requests.html", requests=mapped_requests)
