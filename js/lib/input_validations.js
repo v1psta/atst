476eb042c186d2549b76c6ea97363224d67acd4f
Andrew Croce <andrew+dds@promptworks.com>
2018-08-08 10:59:28 -0400

    input validations lib

--- js/lib/input_validations.js
+++ js/lib/input_validations.js
@@ -1,12 +1,20 @@
+import createNumberMask from 'text-mask-addons/dist/createNumberMask'
+import emailMask from 'text-mask-addons/dist/emailMask'
+
 export default {
   anything: {
-    mask: [],
-    unmask: null,
-    match: /^(?!\s*$).+/
+    mask: false,
+    match: /^(?!\s*$).+/,
+    unmask: [],
   },
   dollars: {
-    mask: ['$','/^\d+/','.','/^\d+/'],
-    unmask: ['$',','],
-    match: /^-?\d+\.?\d*$/
+    mask: createNumberMask({ prefix: '$', allowDecimal: true }),
+    match: /^-?\d+\.?\d*$/,
+    unmask: ['$',',']
+  },
+  email: {
+    mask: emailMask,
+    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    unmask: [],
   }
 }
