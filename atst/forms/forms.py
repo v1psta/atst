ac4cf5279e24c4f484cd319cfc129e56153d79df
richard-dds <richard-ctr@friends.dds.mil>
2018-08-14 15:58:18 -0400

    Don't include csrf_token in form.data

--- atst/forms/forms.py
+++ atst/forms/forms.py
@@ -6,3 +6,9 @@ class ValidatedForm(FlaskForm):
         """Performs any applicable extra validation. Must
         return True if the form is valid or False otherwise."""
         return True
+
+    @property
+    def data(self):
+        _data = super().data
+        _data.pop("csrf_token", None)
+        return _data
