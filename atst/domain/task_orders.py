42325d5a2834113959c018f4e5993852de0a9602
dandds <dan-ctr@friends.dds.mil>
2018-08-20 16:09:08 -0400

    TaskOrders domain class can query EDA api via client

--- atst/domain/task_orders.py
+++ atst/domain/task_orders.py
@@ -8,12 +8,32 @@ from .exceptions import NotFoundError
 class TaskOrders(object):
 
     @classmethod
-    def get(self, order_number):
+    def get(cls, order_number, client=None):
         try:
             task_order = (
                 db.session.query(TaskOrder).filter_by(number=order_number).one()
             )
         except NoResultFound:
+            if client:
+                task_order = TaskOrders._get_from_eda(order_number, client)
+            else:
+                raise NotFoundError("task_order")
+
+        return task_order
+
+    @classmethod
+    def _get_from_eda(cls, order_number, client):
+        to_data = client.get_contract(order_number, status="y")
+        if to_data:
+            return TaskOrders.create(to_data["contract_no"])
+        else:
             raise NotFoundError("task_order")
+
+    @classmethod
+    def create(cls, order_number):
+        task_order = TaskOrder(number=order_number)
+
+        db.session.add(task_order)
+        db.session.commit()
 
         return task_order
