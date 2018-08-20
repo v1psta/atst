cfc76f513784d9dafe16966aca11ec0e8e04a722
Patrick Smith <patrick+dds@promptworks.com>
2018-08-20 11:00:40 -0400

    Add filter for formatting dollar values

--- atst/filters.py
+++ atst/filters.py
@@ -3,5 +3,14 @@ def iconSvg(name):
         return contents.read()
 
 
+def dollars(value):
+    try:
+        numberValue = float(value)
+    except ValueError:
+        numberValue = 0
+    return "${:,.0f}".format(numberValue)
+
+
 def register_filters(app):
     app.jinja_env.filters['iconSvg'] = iconSvg
+    app.jinja_env.filters['dollars'] = dollars
---
+++ tests/test_filters.py
@@ -0,0 +1,15 @@
+import pytest
+
+from atst.filters import dollars
+
+
+@pytest.mark.parametrize("input,expected", [
+    ('0', '$0'),
+    ('123.00', '$123'),
+    ('1234567', '$1,234,567'),
+    ('-1234', '$-1,234'),
+    ('one', '$0'),
+])
+def test_dollar_fomatter(input, expected):
+    assert dollars(input) == expected
+
