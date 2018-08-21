34d652f33a6dba500b6159bb1f3f04d25b6edeb9
Patrick Smith <patrick+dds@promptworks.com>
2018-08-21 14:52:06 -0400

    Add context processor to put allow accessing workspace in template

--- atst/routes/workspaces.py
+++ atst/routes/workspaces.py
@@ -5,6 +5,13 @@ from atst.domain.workspaces import Members, Projects, Workspaces
 
 bp = Blueprint("workspaces", __name__)
 
+@bp.context_processor
+def workspace():
+    workspace = None
+    if "workspace_id" in http_request.view_args:
+        workspace = Workspaces.get(http_request.view_args["workspace_id"])
+    return { "workspace": workspace }
+
 
 @bp.route("/workspaces")
 def workspaces():
@@ -14,21 +21,15 @@ def workspaces():
 @bp.route("/workspaces/<workspace_id>/projects")
 def workspace_projects(workspace_id):
     projects = Projects.get_many(workspace_id)
-    return render_template(
-        "workspace_projects.html", workspace_id=workspace_id, projects=projects
-    )
+    return render_template("workspace_projects.html", projects=projects)
 
 
 @bp.route("/workspaces/<workspace_id>/members")
 def workspace_members(workspace_id):
     members = Members.get_many(workspace_id)
-    return render_template(
-        "workspace_members.html", workspace_id=workspace_id, members=members
-    )
+    return render_template("workspace_members.html", members=members)
 
 
 @bp.route("/workspaces/<workspace_id>/reports")
 def workspace_reports(workspace_id):
-    return render_template(
-        "workspace_reports.html", workspace_id=workspace_id
-    )
+    return render_template("workspace_reports.html")
