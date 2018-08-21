f8c3ac824a3680c298732d5e5314c5419b5cca21
Patrick Smith <patrick+dds@promptworks.com>
2018-08-21 14:51:10 -0400

    Refactor workspace domain class to match other domain objects

--- atst/routes/workspaces.py
+++ atst/routes/workspaces.py
@@ -1,29 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request as http_request
 
-from atst.domain.workspaces import Projects, Members
+from atst.domain.workspaces import Members, Projects, Workspaces
 
 
 bp = Blueprint("workspaces", __name__)
 
-mock_workspaces = [
-    {
-        "name": "Unclassified IaaS and PaaS for Defense Digital Service (DDS)",
-        "id": "5966187a-eff9-44c3-aa15-4de7a65ac7ff",
-        "task_order": {"number": 123456},
-        "user_count": 23,
-    }
-]
-
 
 @bp.route("/workspaces")
 def workspaces():
-    return render_template("workspaces.html", page=5, workspaces=mock_workspaces)
+    return render_template("workspaces.html", page=5, workspaces=Workspaces.get_many())
 
 
 @bp.route("/workspaces/<workspace_id>/projects")
 def workspace_projects(workspace_id):
-    projects_repo = Projects()
-    projects = projects_repo.get_many(workspace_id)
+    projects = Projects.get_many(workspace_id)
     return render_template(
         "workspace_projects.html", workspace_id=workspace_id, projects=projects
     )
@@ -31,8 +21,7 @@ def workspace_projects(workspace_id):
 
 @bp.route("/workspaces/<workspace_id>/members")
 def workspace_members(workspace_id):
-    members_repo = Members()
-    members = members_repo.get_many(workspace_id)
+    members = Members.get_many(workspace_id)
     return render_template(
         "workspace_members.html", workspace_id=workspace_id, members=members
     )
