f2dbed1ef1a2492573a569586cca4d2f94b79606
dandds <dan-ctr@friends.dds.mil>
2018-08-21 16:07:54 -0400

    add new task order fields

--- atst/models/task_order.py
+++ atst/models/task_order.py
@@ -1,6 +1,19 @@
-from sqlalchemy import Column, Integer, String
+from enum import Enum
+
+from sqlalchemy import Column, Integer, String, Enum as SQLAEnum
 
 from atst.models import Base
+
+class Source(Enum):
+    MANUAL = "Manual"
+    EDA = "eda"
+
+
+class FundingType(Enum):
+    RDTE = "RDTE"
+    OM = "OM"
+    PROC = "PROC"
+    OTHER = "OTHER"
 
 
 class TaskOrder(Base):
@@ -8,3 +21,12 @@ class TaskOrder(Base):
 
     id = Column(Integer, primary_key=True)
     number = Column(String)
+    source = Column(SQLAEnum(Source))
+    funding_type = Column(SQLAEnum(FundingType))
+    funding_type_other = Column(String)
+    clin_0001 = Column(Integer)
+    clin_0003 = Column(Integer)
+    clin_1001 = Column(Integer)
+    clin_1003 = Column(Integer)
+    clin_2001 = Column(Integer)
+    clin_2003 = Column(Integer)
