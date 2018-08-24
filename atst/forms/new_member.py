273d16bf4f5d4b57a3f3046f138f30891eee3e78
richard-dds <richard-ctr@friends.dds.mil>
2018-08-24 15:12:01 -0400

    Build backend form for creating a new workspace member

---
+++ atst/forms/new_member.py
@@ -0,0 +1,30 @@
+from flask_wtf import Form
+from wtforms.fields import StringField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import Required, Email, Length
+
+from atst.forms.validators import IsNumber
+from atst.forms.fields import SelectField
+
+
+class NewMemberForm(Form):
+
+    first_name = StringField(label="First Name", validators=[Required()])
+    last_name = StringField(label="Last Name", validators=[Required()])
+    email = EmailField("Email Address", validators=[Required(), Email()])
+    dod_id = StringField(
+        "DOD ID", validators=[Required(), Length(min=10), IsNumber()]
+    )
+    workspace_role = SelectField(
+        "Workspace Role",
+        choices=[
+            ("", "Select a Role"),
+            ("admin", "Admin"),
+            ("billing_auditor", "Billing Auditor"),
+            ("ccpo", "CCPO"),
+            ("developer", "Developer"),
+            ("owner", "Owner"),
+            ("security_auditor", "Security Auditor"),
+        ],
+        validators=[Required()]
+    )
