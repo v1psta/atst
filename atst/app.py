8af2f5b99e3507d99abc2f15b60e6073bfd297b3
Devon Mackay <devon@dds.mil>
2018-08-30 11:56:46 -0400

    Add ability to override any config setting via ENV var

--- atst/app.py
+++ atst/app.py
@@ -114,6 +114,13 @@ def make_config():
     # ENV_CONFIG will override values in BASE_CONFIG.
     config.read(config_files)
 
+    # Check for ENV variables as a final source of overrides
+    # ENV vars must be named "ATST_<KEYNAME>"
+    for confsetting in config.options("default"):
+        env_override = os.getenv("ATST_" + confsetting.upper())
+        if env_override:
+            config.set("default", confsetting, env_override)
+
     # Assemble DATABASE_URI value
     database_uri = (
         "postgres://"
