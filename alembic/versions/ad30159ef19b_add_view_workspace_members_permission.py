361630e446dc79b0ded7e3ca47a4f308e010a174
richard-dds <richard-ctr@friends.dds.mil>
2018-09-06 10:16:48 -0400

    Use helper methods for adding and removing permissions

--- alembic/versions/ad30159ef19b_add_view_workspace_members_permission.py
+++ alembic/versions/ad30159ef19b_add_view_workspace_members_permission.py
@@ -7,7 +7,6 @@ Create Date: 2018-09-05 11:17:17.204089
 """
 from alembic import op
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.attributes import flag_modified
 
 from atst.models.role import Role
 from atst.models.permissions import Permissions
@@ -24,13 +23,10 @@ def upgrade():
     session = Session(bind=op.get_bind())
 
     owner_role = session.query(Role).filter_by(name="owner").one()
-    owner_role.permissions.append(Permissions.VIEW_WORKSPACE_MEMBERS)
+    owner_role.add_permission(Permissions.VIEW_WORKSPACE_MEMBERS)
 
     ccpo_role = session.query(Role).filter_by(name="ccpo").one()
-    ccpo_role.permissions.append(Permissions.VIEW_WORKSPACE_MEMBERS)
-
-    flag_modified(owner_role, "permissions")
-    flag_modified(ccpo_role, "permissions")
+    ccpo_role.add_permission(Permissions.VIEW_WORKSPACE_MEMBERS)
 
     session.add_all((ccpo_role, owner_role))
     session.commit()
@@ -40,13 +36,10 @@ def downgrade():
     session = Session(bind=op.get_bind())
 
     owner_role = session.query(Role).filter_by(name="owner").one()
-    owner_role.permissions.append(Permissions.VIEW_WORKSPACE_MEMBERS)
+    owner_role.remove_permission(Permissions.VIEW_WORKSPACE_MEMBERS)
 
     ccpo_role = session.query(Role).filter_by(name="ccpo").one()
-    ccpo_role.permissions.append(Permissions.VIEW_WORKSPACE_MEMBERS)
-
-    flag_modified(owner_role, "permissions")
-    flag_modified(ccpo_role, "permissions")
+    ccpo_role.remove_permission(Permissions.VIEW_WORKSPACE_MEMBERS)
 
     session.add_all((ccpo_role, owner_role))
     session.commit()
