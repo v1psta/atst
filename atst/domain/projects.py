9c9374541bd9580530dcb6613272d47af07b5d24
Patrick Smith <patrick+dds@promptworks.com>
2018-09-06 11:47:54 -0400

    Add get project method

--- atst/domain/projects.py
+++ atst/domain/projects.py
@@ -1,4 +1,5 @@
 from atst.database import db
+from atst.domain.exceptions import NotFoundError
 from atst.models.project import Project
 from atst.domain.environments import Environments
 
@@ -11,5 +12,19 @@ class Projects(object):
 
         db.session.add(project)
         db.session.commit()
+
+        return project
+
+    @classmethod
+    def get(cls, user, workspace, project_id):
+        # TODO: this should check permission for this particular project
+        Authorization.check_workspace_permission(
+            user, workspace, Permissions.VIEW_APPLICATION_IN_WORKSPACE,
+        )
+
+        try:
+            project = db.session.query(Project).filter_by(id=project_id).one()
+        except NoResultFound:
+            raise NotFoundError("project")
 
         return project
