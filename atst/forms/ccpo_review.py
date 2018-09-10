041cfe878ffef18ae959bab853e8aecdbd054409
dandds <dan-ctr@friends.dds.mil>
2018-09-10 14:38:04 -0400

    ensure ccpo review fields are optional, add form error alert to review screen

--- atst/forms/ccpo_review.py
+++ atst/forms/ccpo_review.py
@@ -1,6 +1,6 @@
 from wtforms.fields.html5 import EmailField, TelField
 from wtforms.fields import StringField, TextAreaField
-from wtforms.validators import Email
+from wtforms.validators import Email, Optional
 
 from .forms import ValidatedForm
 from .validators import Alphabet, PhoneNumber
@@ -8,11 +8,19 @@ from .validators import Alphabet, PhoneNumber
 
 class CCPOReviewForm(ValidatedForm):
     comment = TextAreaField("Comments (optional)")
-    fname_mao = StringField("First Name (optional)", validators=[Alphabet()])
-    lname_mao = StringField("Last Name (optional)", validators=[Alphabet()])
-    email_mao = EmailField("Mission Owner e-mail (optional)", validators=[Email()])
-    phone_mao = TelField(
-        "Mission Owner phone number (optional)", validators=[PhoneNumber()]
+    fname_mao = StringField(
+        "First Name (optional)", validators=[Optional(), Alphabet()]
     )
-    fname_ccpo = StringField("First Name (optional)", validators=[Alphabet()])
-    lname_ccpo = StringField("Last Name (optional)", validators=[Alphabet()])
+    lname_mao = StringField("Last Name (optional)", validators=[Optional(), Alphabet()])
+    email_mao = EmailField(
+        "Mission Owner e-mail (optional)", validators=[Optional(), Email()]
+    )
+    phone_mao = TelField(
+        "Mission Owner phone number (optional)", validators=[Optional(), PhoneNumber()]
+    )
+    fname_ccpo = StringField(
+        "First Name (optional)", validators=[Optional(), Alphabet()]
+    )
+    lname_ccpo = StringField(
+        "Last Name (optional)", validators=[Optional(), Alphabet()]
+    )
