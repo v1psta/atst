905226e98ad9060b38ec26efc630afbbe80b1e56
Andrew Croce <andrew+dds@promptworks.com>
2018-09-13 12:44:53 -0400

    change label and description for review comments field

--- atst/forms/ccpo_review.py
+++ atst/forms/ccpo_review.py
@@ -7,7 +7,7 @@ from .validators import Alphabet, PhoneNumber
 
 
 class CCPOReviewForm(ValidatedForm):
-    comment = TextAreaField("Comments (optional)")
+    comment = TextAreaField("Instructions or comments", description='Provide instructions or notes for additional information that is necessary to approve the request here. The requestor may then re-submit the updated request or initiate contact outside of AT-AT if further discussion is required. <strong>This message will be shared with the person making the JEDI request.</strong>.')
     fname_mao = StringField(
         "First Name (optional)", validators=[Optional(), Alphabet()]
     )
