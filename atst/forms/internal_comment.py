aaf15ee268840045b3c8fa1aeb9cdd622d03511c
Patrick Smith <patrick+dds@promptworks.com>
2018-09-24 15:34:04 -0400

    Give me some space

--- atst/forms/internal_comment.py
+++ atst/forms/internal_comment.py
@@ -8,6 +8,6 @@ class InternalCommentForm(ValidatedForm):
     text = TextAreaField(
         "CCPO Internal Notes",
         default="",
-        description="Add comments or notes for internal CCPO reference and follow-up here.<strong>These comments <em>will not</em> be visible to the person making the JEDI request.</strong>",
+        description="Add comments or notes for internal CCPO reference and follow-up here. <strong>These comments <em>will not</em> be visible to the person making the JEDI request.</strong>",
         validators=[InputRequired()],
     )
