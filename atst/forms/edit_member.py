90657e213a1f9aa68f90b921ce94ad08a5c9ea3b
Montana <montana-ctr@friends.dds.mil>
2018-09-24 10:08:43 -0400

    Update to FlaskForm

--- atst/forms/edit_member.py
+++ atst/forms/edit_member.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.validators import Optional
 
 from atst.forms.fields import SelectField
@@ -6,7 +6,7 @@ from atst.forms.fields import SelectField
 from .data import WORKSPACE_ROLES, ENVIRONMENT_ROLES
 
 
-class EditMemberForm(Form):
+class EditMemberForm(FlaskForm):
 
     workspace_role = SelectField(
         "Workspace Role", choices=WORKSPACE_ROLES, validators=[Optional()]
