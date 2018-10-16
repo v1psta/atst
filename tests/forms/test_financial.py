f56dd5621eb4912c8b5ffe0964286f1bcee87fef
richard-dds <richard-ctr@friends.dds.mil>
2018-10-16 14:15:27 -0400

    Revive suggest_pe_id tests

--- tests/forms/test_financial.py
+++ tests/forms/test_financial.py
@@ -2,21 +2,21 @@ import pytest
 from werkzeug.datastructures import ImmutableMultiDict
 
 from atst.forms.financial import FinancialForm, ExtendedFinancialForm
-from atst.eda_client import MockEDAClient
+from atst.domain.requests.financial_verification import PENumberValidator
 
 
-# @pytest.mark.parametrize(
-#     "input_,expected",
-#     [
-#         ("0603502N", None),
-#         ("0603502NZ", None),
-#         ("603502N", "0603502N"),
-#         ("063502N", "0603502N"),
-#         ("63502N", "0603502N"),
-#     ],
-# )
-# def test_suggest_pe_id(input_, expected):
-#     assert suggest_pe_id(input_) == expected
+@pytest.mark.parametrize(
+    "input_,expected",
+    [
+        ("0603502N", None),
+        ("0603502NZ", None),
+        ("603502N", "0603502N"),
+        ("063502N", "0603502N"),
+        ("63502N", "0603502N"),
+    ],
+)
+def test_suggest_pe_id(input_, expected):
+    assert PENumberValidator().suggest_pe_id(input_) == expected
 
 
 def test_funding_type_other_not_required_if_funding_type_is_not_other():
