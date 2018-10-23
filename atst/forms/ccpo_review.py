6fdc883a8c6af259ff51f932137a331a0018236e
leigh-mil <leigh-ctr@friends.dds.mil>
2018-10-23 17:21:46 -0400

    Formatting

--- atst/forms/ccpo_review.py
+++ atst/forms/ccpo_review.py
@@ -11,9 +11,7 @@ class CCPOReviewForm(ValidatedForm):
         "Instructions or comments",
         description="Provide instructions or notes for additional information that is necessary to approve the request here. The requestor may then re-submit the updated request or initiate contact outside of AT-AT if further discussion is required. <strong>This message will be shared with the person making the JEDI request.</strong>.",
     )
-    fname_mao = StringField(
-        "First Name (optional)", validators=[Optional(), Name()]
-    )
+    fname_mao = StringField("First Name (optional)", validators=[Optional(), Name()])
     lname_mao = StringField("Last Name (optional)", validators=[Optional(), Name()])
     email_mao = EmailField(
         "Mission Owner e-mail (optional)", validators=[Optional(), Email()]
@@ -21,9 +19,5 @@ class CCPOReviewForm(ValidatedForm):
     phone_mao = TelField(
         "Mission Owner phone number (optional)", validators=[Optional(), PhoneNumber()]
     )
-    fname_ccpo = StringField(
-        "First Name (optional)", validators=[Optional(), Name()]
-    )
-    lname_ccpo = StringField(
-        "Last Name (optional)", validators=[Optional(), Name()]
-    )
+    fname_ccpo = StringField("First Name (optional)", validators=[Optional(), Name()])
+    lname_ccpo = StringField("Last Name (optional)", validators=[Optional(), Name()])
