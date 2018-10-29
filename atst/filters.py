c97ee041cc4ff474c3c78be985585cb03d690d0c
Andrew Croce <andrew+dds@promptworks.com>
2018-10-29 12:25:44 -0400

    format

--- atst/filters.py
+++ atst/filters.py
@@ -76,6 +76,7 @@ def formattedDate(value, formatter="%m/%d/%Y"):
 def dateFromString(value, formatter="%m/%Y"):
     return datetime.datetime.strptime(value, formatter)
 
+
 def string(value):
     return app.jinja_env.filters["string"](value)
 
