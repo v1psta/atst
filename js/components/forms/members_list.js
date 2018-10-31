922a929ce2822c88add07b307b348f76866136f2
Montana <montana-ctr@friends.dds.mil>
2018-10-31 09:15:24 -0400

    Clear filters

--- js/components/forms/members_list.js
+++ js/components/forms/members_list.js
@@ -14,39 +14,20 @@ export default {
     }
   },
 
-  mounted: function () {
-    // this.searchedList = this.members
-  },
-
   computed: {
     searchedList: function () {
       return this.members.filter(
-        member => this.status ? member.status === this.status : true
+        member => this.status ?
+          member.status === this.status | this.status === 'all'
+          : true
       ).filter(
-          member => this.role ? member.role.toLowerCase() === this.role : true
+        member => this.role ?
+          member.role.toLowerCase() === this.role | this.role === 'all'
+          : true
       ).filter(
         member => this.searchValue ? member.name.toLowerCase()
           .includes(this.searchValue.toLowerCase()) : true
       )
     }
-  },
-
-  // watch: {
-  //   status: function (status) {
-  //     this.searchedList = this.searchedList.filter(
-  //       member => member.status === status
-  //     )
-  //   },
-  //   role: function (role) {
-  //     this.searchedList = this.searchedList.filter(
-  //       member => member.role.toLowerCase() === role
-  //     )
-  //   }
-  // },
-
-  methods: {
-    search: function () {
-      console.log("search")
-    },
   },
 }
