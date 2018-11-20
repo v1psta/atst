75155f5297ac7cf97fe8aca16ee7ece5abddc4c4
Patrick Smith <patrick+dds@promptworks.com>
2018-11-19 21:01:51 -0500

    Add initial component test

---
+++ js/components/__tests__/local_datetime.test.js
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils'
+
+import LocalDatetime from '../local_datetime'
+
+
+describe('LocalDatetime', () => {
+  const wrapper = shallowMount(LocalDatetime, { propsData: { timestamp: '1977-05-25 00:00:00' } })
+
+  it('renders a time element', () => {
+    expect(wrapper.html()).toContain('<time')
+    expect(wrapper.html()).toContain('May 25 1977')
+  })
+
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
