33546188d5c38ce6e031794b08797d74ef2420ac
leigh-mil <leigh-ctr@friends.dds.mil>
2018-12-04 17:11:21 -0500

    Refactor to remove repetition

--- js/components/selector.js
+++ js/components/selector.js
@@ -84,8 +84,9 @@ export default {
 
     onShow: function () {
       setTimeout(() => { // timeout is a hack to make focus work in Chrome
-        if (this.$refs.choices.find(choice => choice.selected)) {
-          this.$refs.choices.find(choice => choice.selected).$refs.input[0].focus()
+        const selected = this.$refs.choices.find(choice => choice.selected)
+        if (selected) {
+          selected.$refs.input[0].focus()
         } else {
           this.$refs.choices[0].$refs.input[0].focus()
         }
