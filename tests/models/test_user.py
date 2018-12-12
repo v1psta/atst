38f553b9c330f04fbd87a39030abcb2251a34ec8
dandds <dan-ctr@friends.dds.mil>
2018-12-12 10:36:35 -0500

    add database trigger to ensure dod id is never updated

--- tests/models/test_user.py
+++ tests/models/test_user.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy.exc import InternalError
 
 from atst.models.user import User
 
@@ -15,3 +16,11 @@ def test_profile_complete_with_missing_info(missing_field):
     user = UserFactory.create()
     setattr(user, missing_field, None)
     assert not user.profile_complete
+
+
+def test_cannot_update_dod_id(session):
+    user = UserFactory.create()
+    user.dod_id = "23403498202"
+    session.add(user)
+    with pytest.raises(InternalError):
+        session.commit()
