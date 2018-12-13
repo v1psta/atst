c90550b647f8aba717010c01b0f24dd7ff87c048
dandds <dan-ctr@friends.dds.mil>
2018-12-13 11:38:12 -0500

    resolve migration chain

--- alembic/versions/3dc8b7961bd1_rename_task_orders.py
+++ alembic/versions/3dc8b7961bd1_rename_task_orders.py
@@ -1,7 +1,7 @@
 """rename task orders
 
 Revision ID: 3dc8b7961bd1
-Revises: 02d11579a581
+Revises: c222327c3963
 Create Date: 2018-12-12 13:17:25.728679
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '3dc8b7961bd1'
-down_revision = '02d11579a581'
+down_revision = 'c222327c3963'
 branch_labels = None
 depends_on = None
 
