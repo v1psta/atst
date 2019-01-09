105dc6ef1fa22301fa90b7da4e6046797f414a01
George Drummond <george-ctr@friends.dds.mil>
2019-01-09 11:09:36 -0500

    Cache all calls to translation file and refresh cache when file changes

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = make_app(config)
 
 if __name__ == "__main__":
     port = int(config["PORT"])
-    app.run(port=port)
+    app.run(port=port, extra_files=["translations.yaml"])
     print("Listening on http://localhost:%i" % port)
