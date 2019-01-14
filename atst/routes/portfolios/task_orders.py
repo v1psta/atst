25a34d560852bf5bcc87c6993a241885e9b60ebf
dandds <dan-ctr@friends.dds.mil>
2019-01-14 12:42:46 -0500

    include updated task order page and fix TaskOrders.get call

--- atst/routes/portfolios/task_orders.py
+++ atst/routes/portfolios/task_orders.py
@@ -14,7 +14,7 @@ def portfolio_task_orders(portfolio_id):
 @portfolios_bp.route("/portfolios/<portfolio_id>/task_order/<task_order_id>")
 def view_task_order(portfolio_id, task_order_id):
     portfolio = Portfolios.get(g.current_user, portfolio_id)
-    task_order = TaskOrders.get(task_order_id)
+    task_order = TaskOrders.get(g.current_user, task_order_id)
     return render_template(
         "portfolios/task_orders/show.html", portfolio=portfolio, task_order=task_order
     )
