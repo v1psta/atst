c9639435aa1ed024ea6359b151e850bb6c959463
Patrick Smith <patrick+dds@promptworks.com>
2019-01-15 10:54:39 -0500

    fixup w/ 8f8e7fa6

--- tests/models/test_task_order.py
+++ tests/models/test_task_order.py
@@ -4,27 +4,22 @@ from tests.factories import random_future_date, random_past_date
 
 
 class TestTaskOrderStatus:
-
     def test_pending_status(self):
         to = TaskOrder()
         assert to.status == Status.PENDING
 
-        to = TaskOrder(number='42', start_date=random_future_date())
+        to = TaskOrder(number="42", start_date=random_future_date())
         assert to.status == Status.PENDING
 
     def test_active_status(self):
         to = TaskOrder(
-            number='42',
-            start_date=random_past_date(),
-            end_date=random_future_date(),
+            number="42", start_date=random_past_date(), end_date=random_future_date()
         )
         assert to.status == Status.ACTIVE
 
     def test_expired_status(self):
         to = TaskOrder(
-            number='42',
-            start_date=random_past_date(),
-            end_date=random_past_date(),
+            number="42", start_date=random_past_date(), end_date=random_past_date()
         )
         assert to.status == Status.EXPIRED
 
@@ -33,5 +28,5 @@ def test_is_submitted():
     to = TaskOrder()
     assert not to.is_submitted
 
-    to = TaskOrder(number='42')
+    to = TaskOrder(number="42")
     assert to.is_submitted
