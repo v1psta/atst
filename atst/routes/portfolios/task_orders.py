f6037aa8af013e34a9bda405234695ff4b2e4adf
Patrick Smith <patrick+dds@promptworks.com>
2019-01-15 13:32:05 -0500

    Show pending task orders at top of funding page

--- atst/routes/portfolios/task_orders.py
+++ atst/routes/portfolios/task_orders.py
@@ -1,14 +1,25 @@
+from collections import defaultdict
+
 from flask import g, render_template
 
 from . import portfolios_bp
 from atst.domain.task_orders import TaskOrders
 from atst.domain.portfolios import Portfolios
+from atst.models.task_order import Status as TaskOrderStatus
 
 
 @portfolios_bp.route("/portfolios/<portfolio_id>/task_orders")
 def portfolio_task_orders(portfolio_id):
     portfolio = Portfolios.get(g.current_user, portfolio_id)
-    return render_template("portfolios/task_orders/index.html", portfolio=portfolio)
+    task_orders_by_status = defaultdict(list)
+    for task_order in portfolio.task_orders:
+        task_orders_by_status[task_order.status].append(task_order)
+
+    return render_template(
+        "portfolios/task_orders/index.html",
+        portfolio=portfolio,
+        pending_task_orders=task_orders_by_status.get(TaskOrderStatus.PENDING, []),
+    )
 
 
 @portfolios_bp.route("/portfolios/<portfolio_id>/task_order/<task_order_id>")
