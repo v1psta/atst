ceb67bcb6f6231d7837f25d3ac3ad115c18e247f
Montana <montana-ctr@friends.dds.mil>
2019-01-18 10:12:11 -0500

    Forgot to add new js component file

---
+++ js/components/forms/oversight.js
@@ -0,0 +1,33 @@
+import FormMixin from '../../mixins/form'
+import textinput from '../text_input'
+import checkboxinput from '../checkbox_input'
+
+export default {
+  name: 'oversight',
+
+  mixins: [FormMixin],
+
+  components: {
+    textinput,
+    checkboxinput,
+  },
+
+  props: {
+    initialData: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  data: function () {
+    const {
+      am_cor = false,
+      ko_invite = false
+    } = this.initialData
+
+    return {
+        am_cor,
+        ko_invite
+    }
+  }
+}
