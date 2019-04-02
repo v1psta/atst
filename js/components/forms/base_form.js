8b10732ac9b1f5619e584797db00b2a0fc7ac9d6
leigh-mil <leigh-ctr@friends.dds.mil>
2019-04-02 15:06:36 -0400

    Use base form and save button on all forms

--- js/components/forms/base_form.js
+++ js/components/forms/base_form.js
@@ -4,7 +4,6 @@ import optionsinput from '../options_input'
 import DateSelector from '../date_selector'
 import MultiStepModalForm from './multi_step_modal_form'
 import multicheckboxinput from '../multi_checkbox_input'
-import funding from './funding'
 import checkboxinput from '../checkbox_input'
 import levelofwarrant from '../levelofwarrant'
 
@@ -16,7 +15,6 @@ export default {
     DateSelector,
     MultiStepModalForm,
     multicheckboxinput,
-    funding,
     checkboxinput,
     levelofwarrant,
   },
