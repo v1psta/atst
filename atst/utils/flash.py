5b77d6c0d03300cc1ce0b9543f65451a62cfa811
rachel-dtr <rachel-ctr@friends.dds.mil>
2019-04-04 13:26:40 -0400

    Updating some more capitalization

--- atst/utils/flash.py
+++ atst/utils/flash.py
@@ -10,7 +10,7 @@ MESSAGES = {
     "task_order_draft": {
         "title_template": translate("task_orders.form.draft_alert_title"),
         "message_template": """
-        <p>Please complete your Task Order before submitting it for approval.</p>
+        <p>Please complete your task order before submitting it for approval.</p>
         """,
         "category": "warning",
     },
@@ -24,7 +24,7 @@ MESSAGES = {
     "new_portfolio_member": {
         "title_template": "Success!",
         "message_template": """
-          <p>You have successfully invited {{ new_member.user_name }} to the portfolio admin.</p>
+          <p>You have successfully invited {{ new_member.user_name }} to the portfolio.</p>
         """,
         "category": "success",
     },
@@ -73,7 +73,7 @@ MESSAGES = {
     "portfolio_member_dod_id_error": {
         "title_template": "CAC ID Error",
         "message_template": """
-            The member attempted to accept this invite, but their CAC ID did not match the CAC ID you specified on the invite. Please confirm that the DOD ID is accurate.
+            The member attempted to accept this invite, but their CAC ID did not match the CAC ID you specified on the invite. Please confirm that the DoD ID is accurate.
         """,
         "category": "error",
     },
@@ -114,11 +114,11 @@ MESSAGES = {
             <div class='alert__actions'>
               <a href='{{ url_for("portfolios.show_portfolio", portfolio_id=portfolio.id) }}' class='icon-link'>
                 {{ Icon('shield') }}
-                <span>Go to my Portfolio Home Page</span>
+                <span>Go to my portfolio home page</span>
               </a>
               <a href='#next-steps' class='icon-link'>
                 {{ Icon('arrow-down') }}
-                <span>Review Next Steps Below</span>
+                <span>Review next steps below</span>
               </a>
             </div>
         """,
@@ -127,7 +127,7 @@ MESSAGES = {
     "task_order_incomplete": {
         "title_template": "Task Order Incomplete",
         "message_template": """
-        You must complete your Task Order form before submitting.
+        You must complete your task order form before submitting.
         """,
         "category": "error",
     },
