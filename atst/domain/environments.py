fd84e902e3a9d8263876b0d1a1b60939487b7c80
George Drummond <george-ctr@friends.dds.mil>
2019-05-10 09:57:09 -0400

    Use is rather than ==

--- atst/domain/environments.py
+++ atst/domain/environments.py
@@ -73,7 +73,7 @@ class Environments(object):
     def update_env_role(cls, environment, user, new_role):
         updated = False
 
-        if new_role == None:
+        if new_role is None:
             updated = EnvironmentRoles.delete(user.id, environment.id)
         else:
             env_role = EnvironmentRoles.get(user.id, environment.id)
