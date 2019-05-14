86c6044ca1d98610acd34a7db722800e50773225
Montana <montana-ctr@friends.dds.mil>
2019-05-14 16:53:15 -0400

    Catch None values for IsNumber validator

--- atst/forms/validators.py
+++ atst/forms/validators.py
@@ -32,7 +32,7 @@ def IsNumber(message=translate("forms.validators.is_number_message")):
     def _is_number(form, field):
         try:
             int(field.data)
-        except ValueError:
+        except (ValueError, TypeError):
             raise ValidationError(message)
 
     return _is_number
