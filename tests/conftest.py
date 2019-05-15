9399b34a56d83fe4b320036df88610c0d3288863
richard-dds <richard-ctr@friends.dds.mil>
2019-05-15 11:26:39 -0400

    Autouse notification_sender fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -309,7 +309,7 @@ def mock_logger(app):
     app.logger = real_logger
 
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def notification_sender(app):
     real_notification_sender = app.notification_sender
     app.notification_sender = FakeNotificationSender()
