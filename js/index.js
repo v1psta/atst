a233bf0e0e3e08277fab079fde378dd02828dcc7
richard-dds <richard-ctr@friends.dds.mil>
2019-06-04 14:21:23 -0400

    Remove reference to vue-sticky

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ import ally from 'ally.js'
 import classes from '../styles/atat.scss'
 import Vue from 'vue/dist/vue'
 import VTooltip from 'v-tooltip'
-import VueSticky from 'vue-sticky'
 import stickybits from 'stickybits'
 
 import dodlogin from './components/dodlogin'
