580c6ce3abb45dbc7890ebdc688941a22a30f12f
leigh-mil <leigh-ctr@friends.dds.mil>
2019-06-04 15:51:45 -0400

    Fix rebase

--- tests/test_access.py
+++ tests/test_access.py
@@ -22,8 +22,9 @@ _NO_ACCESS_CHECK_REQUIRED = _NO_LOGIN_REQUIRED + [
     "dev.messages",  # dev tool
     "dev.test_email",  # dev tool
     "portfolios.accept_invitation",  # available to all users; access control is built into invitation logic
-    "portfolios.create_portfolio",  # create a portfolio"portfolios.portfolios",  # the portfolios list is scoped to the user separately
+    "portfolios.create_portfolio",  # create a portfolio
     "portfolios.new_portfolio",  # all users can create a portfolio
+    "portfolios.portfolios",  # the portfolios list is scoped to the user separately
     "task_orders.get_started",  # all users can start a new TO
     "users.update_user",  # available to all users
     "users.user",  # available to all users
