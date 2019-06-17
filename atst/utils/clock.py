85ff3b2cc11636af1b14c75b5e12190efa10693e
George Drummond <george-ctr@friends.dds.mil>
2019-06-17 10:00:09 -0400

    Remove unused Clock.now

--- atst/utils/clock.py
+++ atst/utils/clock.py
@@ -5,7 +5,3 @@ class Clock(object):
     @classmethod
     def today(cls, tz="UTC"):
         return pendulum.today(tz=tz).date()
-
-    @classmethod
-    def now(cls, tz="UTC"):
-        return pendulum.now(tz=tz)
