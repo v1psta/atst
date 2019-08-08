c100cc8d1f570fefbc0445813a58f90fe5abb50f
dandds <dan-ctr@friends.dds.mil>
2019-08-08 08:30:14 -0400

    Move k8s config dir to deploy dir

--- deploy/kubernetes/set_clientca_secret.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl -n atat delete secret atst-config-ini
-kubectl -n atat create secret generic nginx-client-ca-bundle --from-file="${1}"
--- deploy/kubernetes/set_dhparam_secret.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kubectl -n atat delete secret dhparam-4096
-kubectl -n atat create secret generic dhparam-4096 --from-file="${1}"
