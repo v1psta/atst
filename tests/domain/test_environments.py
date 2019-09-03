8d3f488d6dd9a797c497ef6c5a63e61d0bf8c5ff
tomdds <tom.chandler-ctr@friends.dds.mil>
2019-09-03 10:10:11 -0400

    Mark create env test ask skipped

--- tests/domain/test_environments.py
+++ tests/domain/test_environments.py
@@ -15,6 +15,14 @@ from tests.factories import (
 )
 
 
+@pytest.mark.skip(reason="Reinstate and update once jobs api is up")
+def test_create_environments():
+    application = ApplicationFactory.create()
+    environments = Environments.create_many(application, ["Staging", "Production"])
+    for env in environments:
+        assert env.cloud_id is not None
+
+
 def test_update_env_role():
     env_role = EnvironmentRoleFactory.create(role=CSPRole.BASIC_ACCESS.value)
     new_role = CSPRole.TECHNICAL_READ.value
