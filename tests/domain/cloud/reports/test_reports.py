29644a495b2171ec3102b5f9040857055dc6dd8a
graham-dds <graham.beckley-ctr@friends.dds.mil>
2019-12-09 12:06:42 -0500

    Add tests (& placeholder tests) for new reporting

---
+++ tests/domain/cloud/reports/test_reports.py
@@ -0,0 +1,50 @@
+from atst.domain.csp.reports import MockReportingProvider
+
+
+def test_get_environment_monthly_totals():
+    environment = {
+        "name": "Test Environment",
+        "spending": {
+            "this_month": {"JEDI_CLIN_1": 100, "JEDI_CLIN_2": 100},
+            "last_month": {"JEDI_CLIN_1": 200, "JEDI_CLIN_2": 200},
+            "total": {"JEDI_CLIN_1": 1000, "JEDI_CLIN_2": 1000},
+        },
+    }
+    totals = MockReportingProvider._get_environment_monthly_totals(environment)
+    assert totals == {
+        "name": "Test Environment",
+        "this_month": 200,
+        "last_month": 400,
+        "total": 2000,
+    }
+
+
+def test_get_application_monthly_totals():
+    application = {
+        "name": "Test Application",
+        "environments": [
+            {
+                "name": "Z",
+                "spending": {
+                    "this_month": {"JEDI_CLIN_1": 50, "JEDI_CLIN_2": 50},
+                    "last_month": {"JEDI_CLIN_1": 150, "JEDI_CLIN_2": 150},
+                    "total": {"JEDI_CLIN_1": 250, "JEDI_CLIN_2": 250},
+                },
+            },
+            {
+                "name": "A",
+                "spending": {
+                    "this_month": {"JEDI_CLIN_1": 100, "JEDI_CLIN_2": 100},
+                    "last_month": {"JEDI_CLIN_1": 200, "JEDI_CLIN_2": 200},
+                    "total": {"JEDI_CLIN_1": 1000, "JEDI_CLIN_2": 1000},
+                },
+            },
+        ],
+    }
+
+    totals = MockReportingProvider._get_application_monthly_totals(application)
+    assert totals["name"] == "Test Application"
+    assert totals["this_month"] == 300
+    assert totals["last_month"] == 700
+    assert totals["total"] == 2500
+    assert [env["name"] for env in totals["environments"]] == ["A", "Z"]
