6446b4fbd0781cdc0387fd0af14c726935ba530c
leigh-mil <leigh-ctr@friends.dds.mil>
2019-12-11 11:41:47 -0500

    Raise AlreadyExistsError if a task order is created or updated with a number of an existing task order

--- atst/domain/task_orders.py
+++ atst/domain/task_orders.py
@@ -1,9 +1,11 @@
 import datetime
+from sqlalchemy.exc import IntegrityError
 
 from atst.database import db
 from atst.models.clin import CLIN
 from atst.models.task_order import TaskOrder, SORT_ORDERING
 from . import BaseDomainClass
+from .exceptions import AlreadyExistsError
 
 
 class TaskOrders(BaseDomainClass):
@@ -12,9 +14,12 @@ class TaskOrders(BaseDomainClass):
 
     @classmethod
     def create(cls, portfolio_id, number, clins, pdf):
-        task_order = TaskOrder(portfolio_id=portfolio_id, number=number, pdf=pdf)
-        db.session.add(task_order)
-        db.session.commit()
+        try:
+            task_order = TaskOrder(portfolio_id=portfolio_id, number=number, pdf=pdf)
+            db.session.add(task_order)
+            db.session.commit()
+        except IntegrityError:
+            raise AlreadyExistsError("task_order")
 
         TaskOrders.create_clins(task_order.id, clins)
 
@@ -35,7 +40,11 @@ class TaskOrders(BaseDomainClass):
             task_order.number = number
             db.session.add(task_order)
 
-        db.session.commit()
+        try:
+            db.session.commit()
+        except IntegrityError:
+            raise AlreadyExistsError("task_order")
+
         return task_order
 
     @classmethod
