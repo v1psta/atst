6d64665474b0aaa440ed2b8672a56d25dd086f70
Rob Gil <dds@rem5.com>
2019-12-18 18:45:25 -0500

    169163334 - Adds CDN module
    Basic CDN module with configurable origin.

---
+++ terraform/providers/dev/cdn.tf
@@ -0,0 +1,8 @@
+module "cdn" {
+  source = "../../modules/cdn"
+  origin_host_name = "staging.atat.code.mil"
+  owner = var.owner
+  environment = var.environment
+  name = var.name
+  region = var.region
+}
